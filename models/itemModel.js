a7228ea6d388c4e0661f0b211a7ea24ab1987f39
Zak <zakadmaster@gmail.com>
2020-03-28 17:24:35 +0100

    Addition of delete item

--- models/itemModel.js
+++ models/itemModel.js
@@ -2,7 +2,9 @@ const db = require("../data/db-config")
 
 module.exports = {
     findAll,
-    findById
+    findById,
+    add,
+    remove
 }
 
 function findAll() {
@@ -11,4 +13,12 @@ function findAll() {
 
 function findById(id) {
     return db("Items").where({id})
+}
+
+function add(item) {
+    return db("Items").insert(item)
+}
+
+function remove(id) {
+    return db("items").del().where({id})
 }
