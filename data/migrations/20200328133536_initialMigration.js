b198fa4b02205a4771166460d0e6dca475740b3f
Zak <zakadmaster@gmail.com>
2020-03-28 13:58:46 +0100

    Addition of Users & Items tables

--- data/migrations/20200328133536_initialMigration.js
+++ data/migrations/20200328133536_initialMigration.js
@@ -1,12 +1,27 @@
 
 exports.up = function(knex) {
   return knex.schema
-  .createTable("something", tbl => {
+  .createTable("Users", tbl => {
       tbl.increments()
+      tbl.string("username", 20).unique().notNullable()
+      tbl.string("password", 26).notNullable()
+      tbl.string("department", 6).notNullable()
+  })
+  .createTable("Items", tbl => {
+      tbl.increments()
+      tbl.string("item_name", 30).notNullable()
+      tbl.string("description", 144).notNullable()
+      tbl.integer("user_id").notNullable()
+      tbl.boolean("availability").notNullable()
+      tbl.integer("daily_rate").notNullable()
+      tbl.string("condition").notNullable()
+      tbl.string("location").notNullable()
+      tbl.string("imgs")
   })
 };
 
 exports.down = function(knex) {
   return knex.schema
-  .dropTableIfExists("something")
+  .dropTableIfExists("Users")
+  .dropTableIfExists("Items")
 };
